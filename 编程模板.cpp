ada760e32d77a0a655a4771eddff59209599581a
liziming163 <502642808@qq.com>
2021-05-17 09:41:45 +0800

    Add files via upload

---
+++ 编程模板.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin>>a;
+	if(a%2!=0) {
+		cout<<"A";
+	}
+	if(a<4) {
+		cout<<"B";
+	}
+	if(a!=6) {
+		cout<<"C";
+	}
+	return 0;
+}
